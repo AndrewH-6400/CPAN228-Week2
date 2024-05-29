ee745acf53f8d903f1981f1176fe9199b301f91d
drewH7770 <125577538+drewH7770@users.noreply.github.com>
2024-05-29 19:45:00 -0400

    Week 4 Setup & Start

--- src/main/java/com/humber/Week4JDBCApp/repositories/DishRepository.java
+++ src/main/java/com/humber/Week4JDBCApp/repositories/DishRepository.java
@@ -1,50 +1,51 @@
 package com.humber.Week4JDBCApp.repositories;
 
 import com.humber.Week4JDBCApp.models.Dish;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.ArrayList;
 
+@Repository
 public class DishRepository {
 
-    private static List<Dish> dishes = new ArrayList<>();
+    @Autowired
+    JdbcTemplate template;
 
-    static {
-        dishes.add(
-                Dish.builder()
-                        .id(1)
-                        .name("Pizza")
-                        .category("Veg")
-                        .price(10.0)
-                    .build()
-        );
-        dishes.add(
-                Dish.builder()
-                        .id(2)
-                        .name("Burger")
-                        .category("Non-Veg")
-                        .price(5.0)
-                        .build()
-        );
-        dishes.add(
-                Dish.builder()
-                        .id(3)
-                        .name("Shawarma")
-                        .category("Non-Veg")
-                        .price(8.0)
-                        .build()
-        );
-        dishes.add(
-                Dish.builder()
-                        .id(4)
-                        .name("Poutine")
-                        .category("Veg")
-                        .price(12.0)
-                        .build()
-        );
-    }
+    //save a dish
 
-    public static List<Dish> getDishes(){
+
+
+    //get all dishes
+    public List<Dish> getDishes(){
+        String sql = "SELECT * FROM Dish";
+
+        RowMapper<Dish> mapper = new RowMapper<Dish>() {
+
+            @Override
+            public Dish mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Dish myDish = new Dish();
+
+                //call the attribute through either the name of the column of the index
+                //the first column is ID, so we call the first column
+                //rs is the row of the table we are getting results from
+                myDish.setId(rs.getInt(1));
+                myDish.setName(rs.getString(2));
+                myDish.setCategory(rs.getString(3));
+                myDish.setPrice(rs.getDouble(4));
+
+                return myDish;
+            }
+        };
+
+        //mapper loops through the query results to return a list
+        List<Dish> dishes = template.query(sql, mapper);
+
         return dishes;
     }
 }
--- src/main/java/com/humber/Week4JDBCApp/services/DishService.java
+++ src/main/java/com/humber/Week4JDBCApp/services/DishService.java
@@ -2,14 +2,21 @@ package com.humber.Week4JDBCApp.services;
 
 import com.humber.Week4JDBCApp.models.Dish;
 import com.humber.Week4JDBCApp.repositories.DishRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class DishService {
+
+    //inject dependency so that getDishes can be accessed
+    @Autowired
+    private DishRepository dishRepository;
+
     //Add a method to get all the dishes
     public List<Dish> getAllDishes() {
-        return DishRepository.getDishes();
+
+        return dishRepository.getDishes();
     }
 }
