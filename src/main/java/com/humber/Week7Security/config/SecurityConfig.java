7263a160e17772a44042e7faff48f940ff1bf4d9
AndrewH-6400 <drewh6400@gmail.com>
2024-06-19 20:07:27 -0400

    Security Config

---
+++ src/main/java/com/humber/Week7Security/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.humber.Week7Security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        //these are chains
+        http.authorizeHttpRequests((authorize) -> authorize
+                //each route and what role can go where, ** means everything following that path
+                .requestMatchers("/restaurant/home").permitAll()
+                .requestMatchers("/restaurant/menu/**").hasAnyRole("USER","ADMIN")
+                .requestMatchers("/restaurant/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        //let the user login with whatever default form spring security is using
+        ).formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user-one")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+        UserDetails admin1 = User.withUsername("admin-one")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1,admin1);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer ignoreResources(){
+        return(webSecurity) -> webSecurity
+                .ignoring()
+                .requestMatchers("css/**","h2-console/**");
+    }
+}
